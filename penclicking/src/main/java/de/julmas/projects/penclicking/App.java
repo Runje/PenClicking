f9c97772bc783df0321ed90c7a706a54db22edca
Thomas <Thomas@192.168.1.4>
2014-02-12 19:50:49 +0100

    Start Project

---
+++ penclicking/src/main/java/de/julmas/projects/penclicking/App.java
@@ -0,0 +1,13 @@
+package de.julmas.projects.penclicking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
